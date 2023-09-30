9ae2a0d3ed60771c948a1654cc9c9c1081731ea6
Sang Phạm <youlovelong123@gmail.com>
2023-09-30 11:00:13 +0700

    login fail

--- lib/services/api_method/login.dart
+++ lib/services/api_method/login.dart
@@ -2,74 +2,76 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<void> loginToSuiteCRM() async {
-  const String apiUrl = 'http://example.com/suitecrm/service/v4_1/rest.php';
+  const String loginUrl = 'http://example.com/suitecrm/service/v4_1/rest.php';
+  const String getEntryListUrl =
+      'http://example.com/suitecrm/service/v4_1/rest.php';
 
-  final userAuth = {
-    'user_name': 'nv',
-    'password': '7d790f9e30034b34eac3886d31848d28',
-  };
-
-  const appName = 'My SuiteCRM REST Client';
-  final nameValueList = {};
-
-  final args = {
-    'user_auth': userAuth,
-    'application_name': appName,
-    'name_value_list': nameValueList,
-  };
-
-  final response = await http.post(
-    Uri.parse(apiUrl),
-    headers: {
-      'Content-Type': 'application/json',
+  // Đối tượng chứa thông tin đăng nhập
+  final Map<String, dynamic> loginData = {
+    "user_auth": {
+      "user_name": "nv",
+      "password": "7d790f9e30034b34eac3886d31848d28"
     },
-    body: jsonEncode(args),
+    "application_name": "TEST API",
+    "name_value_list": [],
+    "deleted": "0"
+  };
+
+  // Đối tượng chứa yêu cầu get_entry_list cho module "Leads"
+  final Map<String, dynamic> entryListRequest = {
+    'session': '', // Thay thế bằng session ID nhận được từ yêu cầu đăng nhập
+    'module_name': 'Leads',
+    'query': '',
+    'link_name_to_fields_array': [],
+    'order_by': '',
+    'offset': 0,
+    'select_fields': ['id', 'name'],
+    'max_results': 10,
+    'deleted': 0,
+  };
+
+  // Đăng nhập để nhận session ID
+  final loginResponse = await http.post(
+    Uri.parse(loginUrl),
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode({
+      'method': 'login',
+      'input_type': 'JSON',
+      'response_type': 'JSON',
+      'rest_data': loginData
+    }),
   );
 
-  if (response.statusCode == 200) {
-    final Map<String, dynamic> jsonResponse = json.decode(response.body);
-    final sessionId = jsonResponse['id'];
+  if (loginResponse.statusCode == 200) {
+    final Map<String, dynamic> loginJsonResponse =
+        json.decode(loginResponse.body);
+    final String sessionId = loginJsonResponse['id'];
 
-    final entryArgs = {
-      'session': sessionId,
-      'module_name': 'Accounts',
-      'query': "accounts.billing_address_city = 'Ohio'",
-      'order_by': '',
-      'offset': 0,
-      'select_fields': ['id', 'name'],
-      'link_name_to_fields_array': [
-        {
-          'name': 'contacts',
-          'value': ['first_name', 'last_name']
-        }
-      ],
-      'max_results': 10,
-      'deleted': 0,
-    };
+    // Cập nhật session ID trong yêu cầu get_entry_list
+    entryListRequest['session'] = sessionId;
 
-    final entryResponse = await http.post(
-      Uri.parse(apiUrl),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    // Gửi yêu cầu get_entry_list
+    final entryListResponse = await http.post(
+      Uri.parse(getEntryListUrl),
+      headers: {'Content-Type': 'application/json'},
       body: jsonEncode({
         'method': 'get_entry_list',
         'input_type': 'JSON',
         'response_type': 'JSON',
-        'rest_data': entryArgs
+        'rest_data': entryListRequest
       }),
     );
 
-    if (entryResponse.statusCode == 200) {
-      final Map<String, dynamic> entryJsonResponse =
-          json.decode(entryResponse.body);
-      print(entryJsonResponse);
+    if (entryListResponse.statusCode == 200) {
+      final Map<String, dynamic> entryListJsonResponse =
+          json.decode(entryListResponse.body);
+      print(entryListJsonResponse);
     } else {
       print(
-          'Failed to retrieve entry list with status code: ${entryResponse.statusCode}');
+          'Failed to retrieve entry list with status code: ${entryListResponse.statusCode}');
     }
   } else {
-    print('Login Failed with status code: ${response.statusCode}');
+    print('Login Failed with status code: ${loginResponse.statusCode}');
   }
 }
 
