3b798a0aa198650de7437755def804b9dbeeb5d1
Sang Phạm <youlovelong123@gmail.com>
2023-09-30 11:19:16 +0700

    back

--- lib/services/api_method/login.dart
+++ lib/services/api_method/login.dart
@@ -1,71 +1,25 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<void> loginAndFetchLeads() async {
-  const String apiUrl = 'http://example.com/suitecrm/service/v4_1/rest.php';
-
-  final loginData = {
-    'user_auth': {
-      'user_name': 'nv',
-      'password': '7d790f9e30034b34eac3886d31848d28',
+Future<void> main() async {
+  final Map<String, dynamic> loginData = {
+    "user_auth": {
+      "user_name": "nv",
+      "password": "7d790f9e30034b34eac3886d31848d28"
     },
-    'application_name': 'TEST API',
-    'name_value_list': [],
-    'deleted': '0',
+    "application_name": "TEST API",
+    "name_value_list": [],
+    "deleted": "0"
   };
 
-  final loginResponse = await http.post(
-    Uri.parse(apiUrl),
-    headers: {'Content-Type': 'application/json'},
-    body: jsonEncode({
-      'method': 'login',
-      'input_type': 'JSON',
-      'response_type': 'JSON',
-      'rest_data': loginData,
-    }),
+  final response = await http.post(
+    Uri.parse('https://suitecrm8.longphatcrm.vn/service/v4_1/rest.php'),
+    // body: json.encode(loginData),
   );
 
-  if (loginResponse.statusCode == 200) {
-    final Map<String, dynamic> loginJsonResponse =
-        json.decode(loginResponse.body);
-    final String sessionId = loginJsonResponse['id'];
-
-    final entryListRequest = {
-      'session': sessionId,
-      'module_name': 'Leads',
-      'query': '',
-      'link_name_to_fields_array': [],
-      'order_by': '',
-      'offset': 0,
-      'select_fields': ['id', 'name'],
-      'max_results': 10,
-      'deleted': 0,
-    };
-
-    final entryListResponse = await http.post(
-      Uri.parse(apiUrl),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'method': 'get_entry_list',
-        'input_type': 'JSON',
-        'response_type': 'JSON',
-        'rest_data': entryListRequest,
-      }),
-    );
-
-    if (entryListResponse.statusCode == 200) {
-      final Map<String, dynamic> entryListJsonResponse =
-          json.decode(entryListResponse.body);
-      print(entryListJsonResponse);
-    } else {
-      print(
-          'Failed to retrieve entry list with status code: ${entryListResponse.statusCode}');
-    }
+  if (response.statusCode == 200) {
+    // final responseData = json.decode(response.body);
+    print('Login Response: ${response.body}');
   } else {
-    print('Login Failed with status code: ${loginResponse.statusCode}');
+    print('Login Failed with status code: ${response.statusCode}');
   }
-}
-
-void main() async {
-  await loginAndFetchLeads();
 }
