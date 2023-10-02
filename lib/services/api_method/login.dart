a5bd977953e17a4e2c2c623e0300527aece1be2d
Sang Phạm <youlovelong123@gmail.com>
2023-10-02 09:33:19 +0700

    api

--- lib/services/api_method/login.dart
+++ lib/services/api_method/login.dart
@@ -1,25 +1,64 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:crypto/crypto.dart';
 
-Future<void> main() async {
-  final Map<String, dynamic> loginData = {
-    "user_auth": {
-      "user_name": "nv",
-      "password": "7d790f9e30034b34eac3886d31848d28"
-    },
-    "application_name": "TEST API",
-    "name_value_list": [],
-    "deleted": "0"
-  };
+const suiteCrmBaseUrl =
+    'https://suitecrm8.longphatcrm.vn/service/v4_1/rest.php';
 
+Future<Map<String, dynamic>> restRequest(
+    String method, Map<String, dynamic> arguments) async {
   final response = await http.post(
-    Uri.parse('https://suitecrm8.longphatcrm.vn/service/v4_1/rest.php'),
-    // body: json.encode(loginData),
+    Uri.parse(suiteCrmBaseUrl),
+    body: {
+      "method": method,
+      "input_type": "JSON",
+      "response_type": "JSON",
+      "rest_data": jsonEncode(arguments),
+    },
   );
 
-  if (response.statusCode == 200) {
-    // final responseData = json.decode(response.body);
-    print('Login Response: ${response.body}');
-  } else {
-    print('Login Failed with status code: ${response.statusCode}');
+  if (response.statusCode != 200) {
+    throw Exception(
+        'Failed to make request to SuiteCRM REST API: ${response.statusCode}');
   }
+
+  return jsonDecode(response.body);
+}
+
+Future<void> main() async {
+  final userAuth = {
+    'user_name': 'nv',
+    'password': '7d790f9e30034b34eac3886d31848d28',
+  };
+  const appName = 'My SuiteCRM REST Client';
+  final nameValueList = [];
+
+  final loginArgs = {
+    'user_auth': userAuth,
+    'application_name': appName,
+    'name_value_list': nameValueList,
+  };
+
+  final loginResult = await restRequest('login', loginArgs);
+  final sessionId = loginResult['id'];
+
+  final entryArgs = {
+    'session': sessionId,
+    'module_name': 'Accounts',
+    'query': "accounts.billing_address_city = 'Ohio'",
+    'order_by': '',
+    'offset': 0,
+    'select_fields': ['id', 'name'],
+    'link_name_to_fields_array': [
+      {
+        'name': 'contacts',
+        'value': ['first_name', 'last_name']
+      }
+    ],
+    'max_results': 10,
+    'deleted': 0,
+  };
+
+  final entryListResult = await restRequest('get_entry_list', entryArgs);
+  print(entryListResult);
 }
